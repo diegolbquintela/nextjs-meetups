3678a99e353c0c388ed35558c2535dd8d56a8684
Diego Quintela <diegolucasq@gmail.com>
2022-04-22 18:13:00 -0400

    bug fix

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
@@ -6,7 +8,7 @@ function MeetupItem(props) {
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <Image src={props.image} alt={props.title} />
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>
